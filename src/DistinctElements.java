625b3799cb4e2b176a3ab7b16b42f81feb848816
AlexandraBagi <szandra.bagi@gmail.com>
2019-10-31 21:46:07 +0100

    Question 3 - passes CodeJudge

--- src/DistinctElements.java
+++ src/DistinctElements.java
@@ -1,94 +1,52 @@
+import java.io.*;
 import java.util.*;
 
 public class DistinctElements {
 
     private int[] A = {0x21ae4036, 0x32435171, 0xac3338cf, 0xea97b40c, 0x0e504b22, 0x9ff9a4ef, 0x111d014d, 0x934f3787, 0x6cd079bf, 0x69db5c31, 0xdf3c28ed, 0x40daf2ad, 0x82a5891c, 0x4659c7b0, 0x73dc0ca8, 0xdad3aca2, 0x00c74c7e, 0x9a2521e2, 0xf38eb6aa, 0x64711ab6, 0x5823150a, 0xd13a3a9a, 0x30a5aa04, 0x0fb9a1da, 0xef785119, 0xc9f0b067, 0x1e7dde42, 0xdda4a7b2, 0x1a1c2640, 0x297c0633, 0x744edb48, 0x19adce93};
-    private int x;
 
-    DistinctElements(int x){
-        this.x = x;
-    }
+    DistinctElements() {}
 
     /**
      * Exercise 1.
-     * This method returns a integer hashcode for x using A matrix. 
+     * This method returns a integer hashcode for x using A matrix.
      */
-    public int calculateHashValue(){
+    public int calculateHashValue(int x) {
 
         int result = 0;
         int pos = 31;
 
-        for(int i = 0; i < 32; i++) {
+        for (int i = 0; i < 32; i++) {
 
             int temp = x & A[i];
 
             int hashValuePositioni = Integer.bitCount(temp) % 2;
-            //System.out.println(hashValuePositioni);
 
             if ((Integer.bitCount(x & A[i]) % 2) == 1) {
-            if (hashValuePositioni == 1)  {
-                result |= ((hashValuePositioni) << pos);
+                if (hashValuePositioni == 1) {
+                    result |= ((hashValuePositioni) << pos);
+                }
             }
-        }
-        pos--;
+            pos--;
         }
         return result;
     }
 
-    public int rho(int x) {
-        // 2 base logarithm of x
-        int i = (int) (Math.log(x) / Math.log(2));
-        return i;
-    }
+    /*public static void main(String[] args) {
 
-    public double Z(int[] M, int m) {
-        int denominator = 0;
-        for (int i = 0; i < m; i++) {
-            denominator += (int)(Math.pow(2, -M[i]));
-        }
-        if (denominator != 0) return (1 / denominator);
-        else return -1.0;
-    }
+        // Exercise 1
 
-    public int E(int m, int Z) {
-        return (m*m*Z*0.7213/(1+1.079/m));
-    }
+        //Scanner sc = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        //106, . . . , 2 · 106 − 1 with 1 million distinct items: <= but maybe we don't need to have it? 
-     
-        //exercise 2 ( m = 1024 and k = 32). Test the input on 106, . . . , 2 · 106 − 1 with 1 million distinct items: 
-        //How many distinct elements are reported by your implementation? (Add this number to your report.). It also needs to take 
-        HyperLogLogCounter counter = new HyperLogLogCounter(1024,32);
-        int n =counter.hyperLogLogCounter();
-        int threshold = counter.getThreshold(); 
-        if(n<=threshold){
-            System.out.println("above");
-        }else{
-            System.out.println("below");
-        }
-        
-   }
+        //while (sc.hasNextInt()) {
+            //int x = sc.nextInt();
+            //System.out.println("Input number: " + x);
+
+            //DistinctElements de = new DistinctElements(x);
+            // System.out.println("Hash value: " + de.calculateHashValue(x));
+            //System.out.println(de.calculateHashValue());
+    } */
 }
 
-
-
-
-
-
-
-//exercise 1 main
-   // public static void main(String[] args) {
-       // HyperLogLogCounter counter = new HyperLogLogCounter()
-        // Scanner sc = new Scanner(System.in);
-
-        // while(sc.hasNextInt()){
-        //     int x = sc.nextInt();
-        //     // System.out.println("Input number: " + x);
-        
-        //     DistinctElements de = new DistinctElements(x);
-        //     // System.out.println("Hash value: " + de.calculateHashValue(x));
-        //     System.out.println(de.calculateHashValue(x));
-        // }
        
     
--- src/HyperLogLogCounter.java
+++ src/HyperLogLogCounter.java
@@ -1,104 +1,138 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class HyperLogLogCounter{
-    DistinctElements countH; 
-    int m;
-    int k;
-    ArrayList<Integer> allNumbers;
-    int threshold=0;
+    int m; // power of 2, we want to store m integers in M array
+    int k; // 32
+    int[] M;
+    DistinctElements de = new DistinctElements();
 
     HyperLogLogCounter(int m, int k){
-        allNumbers = new ArrayList<>();// stores all the numbers
-        scanInts();
         this.m = m;
         this.k = k;
-      
+        this.M = new int[m]; // for i:=0 to m-1 do M[i]:=0
     }
-    private int rho(int x){
-        int i = (int) (Math.log(x)/ Math.log(2));
-        return i;
-    }
+
+
     private int f(int x){
-        return ((x*0xbc164501) & (0x7fe00000) >> 21);
+        return ((x*0xbc164501) & 0x7fe00000) >> 21; // !!! parenthesis was in the wrong place
     }
-    private double Z(int[] M, int m) {
-        int denominator = 0;
+
+    private double getZ() {
+        double denominator = 0.0;
+        double Z = 0.0;
+
         for (int i = 0; i < m; i++) {
-            denominator += (int)(Math.pow(2, -M[i]));
+            denominator += Math.pow(2, (-M[i]));
         }
-        if (denominator != 0) return (1 / denominator);
-        else return -1.0;
+        if (denominator != 0) Z = 1 /denominator;
+
+        return Z;
     }
 
-    public int E(int m, double Z) {
-        return (int) (m*m*Z* 0.7213/(1 + 1.079/m));
+    public double getE(double Z) {
+        return (m*m*Z* 0.7213/(1 + 1.079/m));
     }
 
-    public int V(int[] M){
-        int zeros =0;
-        for (int i = 0; i<M.length; i++){
-            if (M[i]==0){ zeros+=1;}
-        }
-        return zeros;
-    }
-
-    private void scanInts(){
-        Scanner sc = new Scanner(System.in); 
-        boolean isFirst=true;
-        while(sc.hasNextInt()){
-            if(isFirst){ this.threshold = sc.nextInt();
-            isFirst = false;}
-            else{
-             allNumbers.add(sc.nextInt());
+    // get the number of zeros in M array
+    public int getV(){
+        int V = 0;
+        for (int i = 0; i < M.length; i++){
+            if (M[i] == 0) { 
+                V += 1;
             }
-            
         }
+        return V;
+    }
 
+    // get the position of the first 1 in the binary representation of int read from left
+    public int getRho(int x) {
+        // 2 base logarithm of x if x is positive
+        // !!! negative numbers in binary start with 1
+        // Java uses two's complement for negative 
+        // numbers and the basic rule is to take the positive, invert all bits then add one.
+        int i = 31; // if we have a negative x, we return k-i = 32 - 31 = 1 -> 1st digit is 1
+        if (x >= 0) i = (int) (Math.log(x) / Math.log(2));
+        return k-i;
+    }
+
+    // sequenceElement -> y[i]
+    // numberFromRho -> rho(h(y[i]))
+    public void addSequenceElementToM (int sequenceElement){
+        // j:=f(y[i])
+        int j = f(sequenceElement);
+        // h(y[i])
+        int hashed = de.calculateHashValue(sequenceElement);
+        // rho(h(y[i]))
+        int numberFromRho = getRho(hashed);
+        // M[j]:=max(M[j], rho(h(y[i])))
+        M[j] = Math.max(M[j], numberFromRho);
+        // System.out.println("from max M[j]: " + M[j]);
     }
 
     /**
      *  algorithm1 for estimating the number of distinct integers in a sequence of integers y1,...,yn using an array M of m integers,
      *  where m is a power of 2 with m much smaller than n.
-     * While you are free to use different hash functions, it is suggested that you use h from Exercise 1(calculateHashValue() method) and as f use the following heuristic hash function:
-     * f(x) = ((x*0xbc164501) & 0x7fe00000) >> 21.
+     *  While you are free to use different hash functions, it is suggested that you use h from Exercise 1(calculateHashValue() method) and as f use the following heuristic hash function:
+     *  f(x) = ((x*0xbc164501) & 0x7fe00000) >> 21.
      */
-    public int hyperLogLogCounter(){
-        int[] M = new int[m]; //for i:=0 to m-1 do M[i]:=0
-        int n = allNumbers.size();
-        for(int i=1;i<=n;i++){
-            // j is an index
-            int j = f(allNumbers.get(i-1));
-            countH = new DistinctElements(allNumbers.get(i-1));
-            M[j] =Math.max(M[j], rho(countH.calculateHashValue()));
-         }
-         double Z = Z(M,m); 
-         int V = V(M);
-         int E =E(m,Z);
-         if (E < 2.5*m && V > 0){
-              E= (int) (m * Math.log(m/V));
-         }
-
-        return E;
+    public double hyperLogLogCounter(){
+        // !!! parameter of ln should be a double, it changes the value of E
+        double Z = getZ();
+        // System.out.println("Z: " + Z);
+        //System.out.println(Z);
+        // int V = getV();
+        // System.out.println("V int: " + V);
+        double dV = (double) getV();
+        // System.out.println("V double: " + dV);
+        // double E = getE(Z);
+        double dE = getE(Z);
+        //System.out.println(E);
+        if (dE < 2.5*m && dV > 0){
+            // System.out.println("true");
+            // double parameter = m / V;
+            // System.out.println("Parameter with V as an int: " + String.format("%.6f", parameter));
+            double dParameter = m / dV;
+            // System.out.println("Parameter with V as a double: " + dParameter);
+            // E = m * Math.log(parameter);
+            // System.out.println("E with paraneter as an int: " + String.format("%.6f", E));
+            dE = m * Math.log(dParameter);
+            // System.out.println("E with paraneter as a double: " + String.format("%.6f", dE));
+        }
+        return dE;
     }
-    public int getThreshold(){
-        return threshold;
-    }
 
-    // public static void main(String[] args) {
-    //      //106, . . . , 2 · 106 − 1 with 1 million distinct items: <= but maybe we don't need to have it? 
-      
-    //      //exercise 2 ( m = 1024 and k = 32). Test the input on 106, . . . , 2 · 106 − 1 with 1 million distinct items: 
-    //      //How many distinct elements are reported by your implementation? (Add this number to your report.). It also needs to take 
-    //      HyperLogLogCounter counter = new HyperLogLogCounter(1024,32);
-    //      int n =counter.hyperLogLogCounter();
-    //      int threshold = counter.getThreshold(); 
-    //      if(n>threshold){
-    //          System.out.println("above");
-    //      }else{
-    //          System.out.println("below");
-    //      }
-         
-    // }
+    public static void main(String[] args) {
+        
+        int threshold = 0;
+        HyperLogLogCounter counter = new HyperLogLogCounter(1024, 32);
+        
+        Scanner sc = new Scanner(System.in);
+        boolean isFirst=true;
+        
+        // for i:=1 to n do
+        while(sc.hasNextInt()){
+            if(isFirst){
+                // saving the threshold
+                threshold = sc.nextInt();
+                // System.out.println("Treshold: " + threshold);
+                isFirst = false;
+            } else{
+                // currentNumber -> y[i]
+                int currentNumber = sc.nextInt();
 
+                // add number to M[]
+                counter.addSequenceElementToM(currentNumber);
+                }
+            }
+
+        double count = counter.hyperLogLogCounter();
+
+        if (count < threshold) {
+            System.out.println("below");
+        } else {
+            System.out.println("above");
+        }
+    } 
 }
